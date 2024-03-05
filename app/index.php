61182544af44092328801d3ba88dbaeb8257978a
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-04 20:32:23 -0500

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<?php
+function get_client_ip() {
+  return $_SERVER['HTTP_X_FORWARDED_FOR']
+    ?? $_SERVER['REMOTE_ADDR']
+    ?? $_SERVER['HTTP_CLIENT_IP']
+    ?? '';
+}
+$raw_array = explode(',', get_client_ip());
+$trimmed_array = array_map('trim', $raw_array);
+echo $trimmed_array[0];
+?>
