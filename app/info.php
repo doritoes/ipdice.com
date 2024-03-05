4f2ac9a3472294dfe993072e5ac0b7aeb8459356
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-04 20:55:05 -0500

    Create info.php

---
+++ app/info.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>ipdice.com</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <th>Sourch</th><th>Value</th>
+      </tr>
+      <?php
+         if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+           $raw_array = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+           $trimmed_array = array_map('trim', $raw_array);
+           foreach ($trimmed_array as $ip) {
+             echo "<tr><td>HTTP_X_FORWARDED_FOR</td><td>$ip</td></tr>";
+           }
+         }
+         if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+           echo "<tr><td>REMOTE_ADDR</td><td>$_SERVER['REMOTE_ADDR']</td></tr>";
+         }
+         if (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
+           echo "<tr><td>HTTP_CLIENT_IP</td><td>$_SERVER['HTTP_CLIENT_IP']</td></tr>";
+         }       
+      ?>
+    </table>
+  </body>
+</html>
+<?php
+function get_client_ip() {
+  return $_SERVER['HTTP_X_FORWARDED_FOR']
+    ?? $_SERVER['REMOTE_ADDR']
+    ?? $_SERVER['HTTP_CLIENT_IP']
+    ?? '';
+}
+$raw_array = explode(',', get_client_ip());
+$trimmed_array = array_map('trim', $raw_array);
+echo $trimmed_array[0];
+?>
