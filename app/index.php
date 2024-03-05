10951de6baf97990a85ec062673c456bdbe7a0d2
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-05 08:53:09 -0500

    Update index.php

--- app/index.php
+++ app/index.php
@@ -5,7 +5,26 @@ function get_client_ip() {
     ?? $_SERVER['HTTP_CLIENT_IP']
     ?? '';
 }
-$raw_array = explode(',', get_client_ip());
-$trimmed_array = array_map('trim', $raw_array);
-echo $trimmed_array[0];
+function client_ip() {
+  $raw_ip = get_client_ip();
+  if ($raw_ip) {
+    if (strpos($raw_ip, ",") !== false ) {
+      $raw_array = explode(',', $raw_ip);
+      $trimmed_array = array_map('trim', $raw_array);
+      $ip = $trimmed_array[0];
+    } else {
+      $ip = $raw_ip;
+    }
+  } else {
+    $ip = "IP Address Not Found";
+  }
+  return $ip
+}
+$client_ip = client_ip();
 ?>
+<html>
+  <body>
+    <h1>Your IP Address</h1>
+    <p>{$client_ip}</p>
+  </body>
+</html>
