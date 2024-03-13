9ca4a3726306e614deab03526802373b8650ce51
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-13 15:50:40 -0400

    Create theme.js

---
+++ app/static/scripts/theme.js
@@ -0,0 +1,15 @@
+const triggerObject = document.getElementById("ip-address"); 
+
+function switchTheme() {
+  const themeLink = document.getElementById("theme-stylesheet");
+  const currentTheme = themeLink.getAttribute("href");
+
+  if (currentTheme === "/static/styles/main.css") {
+    themeLink.setAttribute("href", "/static/styles/dark.css"); 
+  } else {
+    themeLink.setAttribute("href", "/static/styles/main.css");
+  }
+}
+
+// Add an event listener to the image
+triggerObject.addEventListener("click", switchTheme);
