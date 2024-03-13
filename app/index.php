c8a1341602a82ced4d0516f7d76774d1d369ee79
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-13 00:04:22 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -41,8 +41,15 @@ $client_ip = validate_ipv4(client_ip());
   <head>
     <title>ipdice.com</title>
     <link rel="stylesheet" href="/static/styles/main.css">
-    <script src="/static/scripts/copy.js" defer></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png">
+    <link rel="manifest" href="/static/styles/site.webmanifest">
+    <link rel="mask-icon" href="/static/images/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <script src="/static/scripts/copy.js" defer></script>
   </head>
   <body>
     <header>
