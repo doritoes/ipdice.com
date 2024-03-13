564ac7219bc22a3d8ba7027c830cd2a074a70b3a
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-13 17:25:41 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -40,7 +40,7 @@ $client_ip = validate_ipv4(client_ip());
 <html lang="en">
   <head>
     <title>ipdice.com</title>
-    <link rel="stylesheet" href="/static/styles/dark.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="/static/styles/main.css" id="theme-stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png">
