69c8e9d1ddc7a87008aa3c5d2f88fa612b0b4f9d
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-13 22:15:27 -0400

    Update index.php

--- app/index.php
+++ app/index.php
@@ -44,7 +44,7 @@ $result = $Browser->getAll($useragent);
 <html lang="en">
   <head>
     <title>ipdice.com</title>
-    <link rel="stylesheet" href="/static/styles/dark.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="/static/styles/main.css" id="theme-stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png">
