3cc08a76601a9d5faa9a3324a8b6bcb077a05a85
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-16 15:03:15 -0400

    Create denied.php

---
+++ app/static/pages/denied.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ipdice.com - Details</title>
+    <link rel="stylesheet" href="/static/styles/denied.css" id="theme-stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png">
+    <link rel="manifest" href="/static/styles/site.webmanifest">
+    <link rel="mask-icon" href="/static/images/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#000000">
+    <script src="/static/scripts/details.js" defer></script>
+  </head>
+  <body>
+    <div class="matrix-container">
+      <div class="matrix-line"></div>
+      <div class="matrix-line"></div>
+      <div class="matrix-line"></div>
+      <div class="matrix-line"></div>
+      <div class="matrix-line"></div>
+      <div class="matrix-line"></div>
+      <div class="ip-display">Access Denied</div>
+  </div>
+  </body>
+</html>
