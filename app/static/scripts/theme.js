50f5f10af1a0b7466a82fd5475bdc0ec854bee51
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-16 15:23:54 -0400

    Update theme.js

--- app/static/scripts/theme.js
+++ app/static/scripts/theme.js
@@ -1,9 +1,9 @@
 const triggerObject = document.getElementById("dice-text"); 
+const triggerLink = document.getElemementById('image');
 
 function switchTheme() {
   const themeLink = document.getElementById("theme-stylesheet");
   const currentTheme = themeLink.getAttribute("href");
-
   if (currentTheme === "/static/styles/main.css") {
     themeLink.setAttribute("href", "/static/styles/dark.css"); 
   } else {
@@ -11,5 +11,9 @@ function switchTheme() {
   }
 }
 
-// Add an event listener to the image
+function switchPage() {
+  window.location.href = '/static/pages/details.php';
+}
+
 triggerObject.addEventListener("click", switchTheme);
+triggerLink.addEventListener("click", switchPage);
