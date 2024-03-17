b37933aa27d93960385337f240258ec570f05c96
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-17 17:52:46 -0400

    Update details.php

--- app/static/pages/details.php
+++ app/static/pages/details.php
@@ -71,7 +71,6 @@ if ($_GET['ip'] !== $ip_address) {
     <script src="/static/scripts/detect.js" defer></script>
     <script src="/static/scripts/details.js" defer></script>
     <script src="/static/scripts/fingerprint.js" defer></script>
-    <script src="/static/scripts/donottrack.js" defer></script>
   </head>
   <body>
     <div class="matrix-container">
