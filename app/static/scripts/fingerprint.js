513d4f37c10db9439497423f3801a9c5f74ec410
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-17 17:01:05 -0400

    Update fingerprint.js

--- app/static/scripts/fingerprint.js
+++ app/static/scripts/fingerprint.js
@@ -1,6 +1,11 @@
-FingerprintJS.load()
-  .then(fp => fp.get())
-  .then(result => {
-    const visitorId = result.visitorId;
-    console.log(JSON.stringify(result, null, 2)); 
-  });
+const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4')
+    .then(FingerprintJS => FingerprintJS.load())
+
+fpPromise
+    .then(fp => fp.get())
+    .then(result => {
+      // This is the visitor identifier:
+      const visitorId = result.visitorId
+      console.log(visitorId)
+      console.log(JSON.stringify(result, null, 2));
+    })
