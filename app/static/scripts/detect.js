92d1add17f44519a09d0fa4b6252e6c6af390702
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-16 21:14:56 -0400

    Update detect.js

--- app/static/scripts/detect.js
+++ app/static/scripts/detect.js
@@ -1,29 +1,39 @@
 const mutationObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (!mutation.target.classList.contains('matrix-line')) {
-          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) { 
-            console.log("Nodes added:", mutation.addedNodes);
-          }    
-        }
-    });
+  mutations.forEach((mutation) => {
+    if (!mutation.target.classList.contains('matrix-line')) {
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) { 
+        console.log("Nodes added:", mutation.addedNodes);
+      }    
+    }
+    const allElements = document.getElementsByTagName('*');
+    const elementsWithNord = [];
+    for (let i = 0; i < allElements.length; i++) {
+      const element = allElements[i];
+      if (element.id && element.id.toLowerCase().includes('nord') ||
+        element.name && element.name.toLowerCase().includes('nord') || 
+        element.classList.contains('nord')) { 
+        elementsWithNord.push(element);
+      }
+    }
+  });
 });
 
 mutationObserver.observe(document.body, { 
-    childList: true, 
-    subtree: true,  
-    attributes: true 
+  childList: true, 
+  subtree: true,  
+  attributes: true 
 });
 
 const allElements = document.getElementsByTagName('*');
 const elementsWithNord = [];
 
 for (let i = 0; i < allElements.length; i++) {
-    const element = allElements[i];
-    if (element.id && element.id.toLowerCase().includes('nord') || // Add guard clause
-        element.name && element.name.toLowerCase().includes('nord') || 
-        element.classList.contains('nord')) { 
-        elementsWithNord.push(element);
-    }
+  const element = allElements[i];
+  if (element.id && element.id.toLowerCase().includes('nord') || // Add guard clause
+    element.name && element.name.toLowerCase().includes('nord') || 
+    element.classList.contains('nord')) { 
+    elementsWithNord.push(element);
+  }
 }
 
 console.log(elementsWithNord); 
