e1379d9d6faa5a7106b969b46b216dcc839970f2
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-21 20:54:06 -0400

    Update details.php

--- app/details.php
+++ app/details.php
@@ -43,11 +43,6 @@ if ($_GET['ip'] != $ip_address) {
   header("Location: $newURL");
   exit;
 }
-if ((!isset($_SERVER['HTTP_REFEFER']) || !$_SERVER['HTTP_REFEFER']) && !is_rfc1918_ip($ip_address)) {
-  $newURL = $scheme . '://' . $_SERVER['HTTP_HOST'] ."/static/pages/denied.php";
-  header("Location: $newURL");
-  exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
