da73550993901df832d3c83cf0a678e3d06ae1b1
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-23 13:33:04 -0400

    Update location.js

--- app/static/scripts/location.js
+++ app/static/scripts/location.js
@@ -25,7 +25,7 @@ function displayLocationData(locationData) {
 }
 
 // Get the user's IP address
-const ipAddress = document.querySelector('.ip-display').textContent;
+const ipAddress = document.getElementById('ip-address').textContent;
 
 // Fetch data and display on success
 fetchLocationData(ipAddress)
