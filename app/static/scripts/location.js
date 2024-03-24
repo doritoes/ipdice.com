e6e9726365ebc0afd3dc928531cdff41cd9e66a2
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-23 22:12:31 -0400

    Update location.js

--- app/static/scripts/location.js
+++ app/static/scripts/location.js
@@ -11,6 +11,9 @@ async function fetchLocationData(ip) {
 
 // Function to create and display the details element
 function displayLocationData(locationData) {
+  if ($locationData.status == 'fail') {
+    return;
+  }
   const city = document.createElement('div');
   city.textContent = `City: ${locationData.city}`;
   city.classList.add('details');
