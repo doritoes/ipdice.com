bfde920ecd73f76827affbd66b0e64073b61c762
doritoes <39832079+doritoes@users.noreply.github.com>
2024-03-24 10:24:26 -0400

    Update location.js

--- app/static/scripts/location.js
+++ app/static/scripts/location.js
@@ -1,6 +1,6 @@
 // Function to fetch location data
-async function fetchLocationData(ip) {
-  const response = await fetch(`http://ip-api.com/json/${ip}?fields=status,message,city,regionName,isp,org`);
+async function fetchLocationData(ipi, apikey) {
+  const response = await fetch(`https://pro.ip-api.com/json/${ip}?fields=status,message,city,regionName,isp,org&key=${apikey}`);
   if (response.ok) { // Check if the request was successful
     const locationData = await response.json();
     return locationData;
@@ -40,8 +40,10 @@ function displayLocationData(locationData) {
 
 // Get the user's IP address
 const ipAddress = document.getElementById('ip-address').textContent;
+// Authentication
+const key = atob("VnNDUXlkTG10SHhUbU9q");
 // Fetch data and display on success
-fetchLocationData(ipAddress)
+fetchLocationData(ipAddress, key)
   .then(locationData => {
     if (locationData) {  // Proceed if we have data
       displayLocationData();
